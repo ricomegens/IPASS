03849cebec6ea9278a44301cf3666128924eef46
ricomegens <rico.megens@gmail.com>
2023-06-22 14:18:36 +0200

    removed methods because they were also in poker

--- player.py
+++ player.py
@@ -5,7 +5,6 @@ class Player:
         self.name = name
         self.hand = []
         self.money = 0
-        self.poker = pl.Poker()
 
     def update_money(self, value):
         self.money += value
@@ -15,24 +14,6 @@ class Player:
 
     def reset_hand(self):
         self.hand.clear()
-
-    def check(self):
-        self.poker.update_pot(self.poker.current_bet)
-        self.update_money(-self.poker.current_bet)
-        return
-
-    def fold(self):
-        self.poker.remove_player(self)
-        self.hand.clear()
-
-    def raise_pot(self, value):
-        if type(value) != int or type(value) != float:
-            print("Wrong input")
-            return False
-        self.poker.update_pot(self.poker.current_bet + value)
-        self.poker.current_bet += value
-        self.update_money(-(self.poker.current_bet + value))
-        return
 
 if __name__ == "__main__":
     player1 = Player("Rico")
