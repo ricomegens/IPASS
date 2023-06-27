659cc2294e7b2d2b08c4331b571196b45883bcf1
ricomegens <rico.megens@gmail.com>
2023-06-27 19:02:29 +0200

    updated hand potential

--- effective_hand_strength.py
+++ effective_hand_strength.py
@@ -1,6 +1,7 @@
 import player
 import poker
-import hands
+import cards_to_come
+import cards
 import evaluate
 
 class EHS:
@@ -10,15 +11,17 @@ class EHS:
         self.deck = deck
 
     def effective_hand_strength(self):
-        return self.hand_strength() \
-                   * (1 - self.hand_potential()[1])\
-                   + (1 - self.hand_strength()) \
-                   * self.hand_potential()[0]
+        hs = self.hand_strength()
+        hp = self.hand_potential()
+        return hs \
+                   * (1 - hp[1])\
+                   + (1 - hs) \
+                   * hp[0]
 
     def hand_strength(self):
         ahead = tied = behind = 0
         ourrank = evaluate.rank(self.hand, self.comm_cards)
-        for oppcards in hands.opp_starts(self.hand, self.comm_cards, self.deck):
+        for oppcards in cards_to_come.opp_starts(self.hand, self.comm_cards, self.deck):
             opprank = evaluate.rank(oppcards, self.comm_cards)
             if ourrank > opprank:
                 ahead += 1
@@ -35,38 +38,47 @@ class EHS:
         HPTotal = [0, 0, 0]  # Initialize to 0
         ourrank = evaluate.rank(self.hand, self.comm_cards)
 
-        for oppcards in hands.opp_starts(self.hand, self.comm_cards, self.deck):
+        all_opp_starts = cards_to_come.opp_starts(self.hand, self.comm_cards, self.deck)
+        for oppcards in all_opp_starts:
             opprank = evaluate.rank(oppcards, self.comm_cards)
-            if ourrank > opprank:
+            if ourrank[0] > opprank[0]:
                 index = 0  # ahead
-            elif ourrank == opprank:
-                index = 1  # tied
+            elif ourrank[0] == opprank[0]:
+                if ourrank[1] > opprank[1]:
+                    index = 0
+                elif ourrank[1] == opprank[1]:
+                    index = 1
+                else:
+                    index = 2
             else:
                 index = 2  # behind
             HPTotal[index] += 1
 
-            for future_board_cards in hands.potential_full_table(self.comm_cards, self.hand, self.deck):
-                ourbest = evaluate.rank(self.hand, future_board_cards)
-                oppbest = evaluate.rank(oppcards, future_board_cards)
-                if ourbest > oppbest:
-                    HP[index][0] += 1  # ahead
-                elif ourbest == oppbest:
-                    HP[index][1] += 1  # tied
+            remaing_cards = cards_to_come.cards_left(self.hand, oppcards, self.comm_cards, self.deck)
+            ourbest = evaluate.rank(self.hand, remaing_cards)
+            oppbest = evaluate.rank(oppcards, remaing_cards)
+            if ourbest[0] > oppbest[0]:
+                HP[index][0] += 1  # ahead
+            elif ourbest[0] == oppbest[0]:
+                if ourbest[1] > oppbest[1]:
+                    HP[index][0] += 1
+                elif ourbest[1] == oppbest[1]:
+                    HP[index][1] += 1
                 else:
-                    HP[index][2] += 1  # behind
+                    HP[index][2] += 1
+            else:
+                HP[index][2] += 1  # behind
 
         Ppot = (HP[2][0] + HP[2][1] / 2 + HP[1][0] / 2) / (HPTotal[2] + HPTotal[1])
         Npot = (HP[0][2] + HP[1][2] / 2 + HP[0][1] / 2) / (HPTotal[0] + HPTotal[1])
-
         return Ppot, Npot
 
-
 if __name__ == "__main__":
-    player1 = player.Player("Rico")
-    player2 = player.Player("Luffy")
-    game = poker.Poker([player1, player2])
-    dk = game.create_deck()
-    player_hand = [dk[0], dk[4]]
+    dk = cards.deck()
+    player_hand = [dk[14], dk[23]]
+    player2_hand = [dk[37], dk[38]]
     community = [dk[13], dk[21], dk[40]]
-    ehs = EHS(player_hand, community, dk)
-    print(ehs.effective_hand_strength())
+    ehs1 = EHS(player_hand, community, dk)
+    ehs2 = EHS(player2_hand, community, dk)
+    print(ehs1.effective_hand_strength())
+    print(ehs2.effective_hand_strength())
