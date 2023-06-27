72fb5e4eb4b1bd25cdc4f4d82b4540b8196e9d71
ricomegens <rico.megens@gmail.com>
2023-06-27 22:01:14 +0200

    moved hand calculator to algrithm file

--- algorithm.py
+++ algorithm.py
@@ -1,114 +1,65 @@
-import effective_hand_strength
-from itertools import combinations
-import poker
+import cards_to_come
+import evaluate
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.chance_node = None
-        self.node_after_chance = None
-        self.l_edge = None
-        self.r_edge = None
+class Expectiminimax:
+    def __init__(self, player, comm_cards):
+        self.hand = player.hand
+        self.comm_cards = comm_cards
 
-    def node_r(self, player):
-        alg = algorithm_flop.EHS(player)
-        value = alg.ehs
-        return value
+    def effective_hand_strength(self):
+        HP = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]  # Hand potential array, initialize to 0
+        HPTotal = [0, 0, 0]  # Initialize to 0
+        ourrank = evaluate.rank(self.hand, self.comm_cards)
 
-    def node_l(self, player):
-        alg = effective_hand_strength.EHS(player)
-        value = alg.ehs
-        value -= potOdd()
-        return abs(value)
+        all_opp_starts = cards_to_come.opp_starts(self.hand, self.comm_cards)
+        for oppcards in all_opp_starts:
+            opprank = evaluate.rank(oppcards, self.comm_cards)
+            if ourrank[0] > opprank[0]:
+                index = 0  # ahead
+            elif ourrank[0] == opprank[0]:
+                if ourrank[1] > opprank[1]:
+                    index = 0
+                elif ourrank[1] == opprank[1]:
+                    index = 1
+                else:
+                    index = 2
+            else:
+                index = 2  # behind
+            HPTotal[index] += 1
 
-class Move:
-    def __init__(self, move, value):
-        self.move = move
-        self.value = value
+            remaing_cards = cards_to_come.cards_left(self.hand, oppcards, self.comm_cards)
+            ourbest = evaluate.rank(self.hand, remaing_cards)
+            oppbest = evaluate.rank(oppcards, remaing_cards)
+            if ourbest[0] > oppbest[0]:
+                HP[index][0] += 1  # ahead
+            elif ourbest[0] == oppbest[0]:
+                if ourbest[1] > oppbest[1]:
+                    HP[index][0] += 1
+                elif ourbest[1] == oppbest[1]:
+                    HP[index][1] += 1
+                else:
+                    HP[index][2] += 1
+            else:
+                HP[index][2] += 1  # behind
 
-class Tree:
-    # players is variable but still hard coded to 2 players
-    def __init__(self, players):
-        self.players = players
+        hand_strength = (HPTotal[0] + 0.5 * HPTotal[1]) / sum(number for number in HPTotal)
+        Ppot = (HP[2][0] + HP[2][1] / 2 + HP[1][0] / 2) / (HPTotal[2] + HPTotal[1])
+        Npot = (HP[0][2] + HP[1][2] / 2 + HP[0][1] / 2) / (HPTotal[0] + HPTotal[1])
+        result = hand_strength * (1 - Npot) + (1 - hand_strength) * Ppot
+        situations_win = 0
+        situations_lose = 0
+        situations_tie = 0
+        for future in HP:
+            situations_win += future[0]
+            situations_tie += future[1]
+            situations_lose += future[2]
+        return result, situations_win, situations_tie, situations_lose
 
-    def make_tree(self):
-        potential_starts = Node(len(list(combinations(poker_class.deck, 2))))
-        is_max_turn = Node(None)
-        potential_starts.node_after_chance = is_max_turn
-
-        fold1_1 = Node(Node.node_l(self.players[0]))
-        play_1_1 = Node(Node.node_r(self.players[0]))
-        is_max_turn.l_edge = fold1_1
-        is_max_turn.r_edge = play_1_1
-
-        fold2_1 = Node(Node.node_l(self.players[1]))
-        play2_1 = Node(Node.node_r(self.players[1]))
-        play_1_1.l_edge = fold2_1
-        play_1_1.r_edge = play2_1
-
-        potential_flops = Node(len(list(combinations(poker_class.deck, 3))))
-        play2_1.chance_node = potential_flops
-        is_max_turn1 = Node(None)
-        potential_flops.node_after_chance = is_max_turn1
-
-        fold1_2 = Node(Node.node_l(self.players[0]))
-        play1_2 = Node(Node.node_r(self.players[0]))
-        is_max_turn1.r_edge = play1_2
-        is_max_turn1.l_edge = fold1_2
-
-        fold2_2 = Node(Node.node_l(self.players[1]))
-        play2_2 = Node(Node.node_r(self.players[1]))
-        play1_2.r_edge = play2_2
-        play1_2.l_edge = fold2_2
-
-        potential_turns = Node(len(list(combinations(poker_class.deck, 2))))
-        play1_2.chance_node = potential_turns
-        is_max_turn2 = Node(None)
-        potential_turns.node_after_chance = is_max_turn2
-
-        fold1_3 = Node(Node.node_l(self.players[0]))
-        play1_3 = Node(Node.node_r(self.players[0]))
-        is_max_turn2.r_edge = play1_3
-        is_max_turn2.l_edge = fold1_3
-
-        fold2_3 = Node(Node.node_l(self.players[1]))
-        play2_3 = Node(Node.node_r(self.players[1]))
-        play1_3.r_edge = play2_3
-        play1_3.l_edge = fold2_3
-
-        potential_river = Node(len(list(combinations(poker_class.deck, 1))))
-        play2_3.chance_node = potential_river
-        is_max_turn3 = Node(None)
-        potential_river.node_after_chance = is_max_turn3
-
-        fold1_4 = Node(Node.node_l(self.players[0]))
-        play1_4 = Node(Node.node_r(self.players[0]))
-        is_max_turn3.r_edge = play1_4
-        is_max_turn3.l_edge = fold1_4
-
-        fold2_4 = Node(Node.node_l(self.players[1]))
-        play2_4 = Node(Node.node_r(self.players[1]))
-        play1_4.r_edge = play2_4
-        play1_4.l_edge = fold2_4
-
-def expectiminimax(node):
-    if node.l_edge is None and node.r_edge is None \
-            and node.chance_node is None and node.node_after_chance is not None:
-        return node.value
-
-    fold = node.l_edge
-    play = node.r_edge
-
-    if fold > play:
-        return Move("fold", fold)
-
-    else:
-        return Move("play", play)
-
-def potOdd():
-    return poker_class.current_bet / (poker_class.pot + poker_class.current_bet)
-
-
-if __name__ == "__main__":
-    poker_class = poker.Poker()
-    game_tree = Tree(["Rico", "Luffy"])
+    def expectiminimax(self):
+        chance = EHS(self.hand, self.comm_cards).effective_hand_strength()[0]
+        if chance > 0.8:
+            return "raise"
+        elif chance > 0.5:
+            return "check"
+        else:
+            return "fold"
