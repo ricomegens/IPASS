edb5622af424e29f983d776a88853aa49badb415
ricomegens <rico.megens@gmail.com>
2023-06-28 01:16:14 +0200

    added some comments

--- cards.py
+++ cards.py
@@ -7,18 +7,23 @@ class Card:
         self.ranking = ranking
 
 def deck():
+    """
+        Function that created class object, adds them to a list and
+        returns a shuffled list of classes object
+
+        Returns:
+        list: Returns shuffled list
+    """
     suits = {"h", "d", "c", "s"}
     values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
     full_deck = []
     for suit in suits:
+        # for each value that we go up in the list, add 1 to the ranking and create a Card object with
+        # corresponding suit, value and ranking that we can add to a list
         ranking = 2
         for value in values:
             full_deck.append(Card(suit, value, ranking))
             ranking += 1
+    # Shuffle the list and return it
     random.shuffle(full_deck)
     return full_deck
-
-
-
-if __name__ == "__main__":
-    print(deck())
