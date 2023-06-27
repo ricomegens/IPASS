3361a8356ece62bcede820b21dd0948b46478664
ricomegens <rico.megens@gmail.com>
2023-06-27 14:20:52 +0200

    rename to more suited name and updated functions with

---
+++ cards_to_come.py
@@ -0,0 +1,29 @@
+from itertools import combinations
+import cards
+
+def opp_starts(own_cards, comm_cards, deck):
+    known = own_cards + comm_cards
+    for card in known:
+        if card in deck:
+            deck.remove(card)
+    combis = list(combinations(deck, 2))
+    combi = [list(combi) for combi in combis]
+    return combi
+
+def potential_full_table(own_cards, opp_cards, comm_cards, deck):
+    amount_of_cards_to_come = 5 - len(comm_cards)
+    known_cards = own_cards + opp_cards + comm_cards
+    for card in known_cards:
+        if card in deck:
+            deck.remove(card)
+    cards_to_come = list(combinations(deck, amount_of_cards_to_come))
+    combis = [list(card) for card in cards_to_come]
+    return combis
+
+
+if __name__ == "__main__":
+    dk = cards.deck()
+    player_hand = [dk[30], dk[8]]
+    community = [dk[12], dk[49], dk[21]]
+    opp = opp_starts(player_hand, community, dk)
+    print(len(potential_full_table(player_hand, opp[0], community, dk)))
