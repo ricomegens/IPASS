d6286c62241a5fd18ca25d43939fc6a32301809d
ricomegens <rico.megens@gmail.com>
2023-06-28 13:05:32 +0200

    added parameter so it correctly removes known cards

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,8 @@
 import cards_calculator
 import evaluate
+import cards
 
-def hand_strength(hand, comm_cards):
+def hand_strength(hand, comm_cards, deck):
     """
         Function that calculates and returns how your hand stands in the game with
         a number representing between 0 and 1 and the amount of hands you beat, lose
@@ -10,6 +11,7 @@ def hand_strength(hand, comm_cards):
         Args:
         hand (list): list of members of class Card that belong to own hand
         comm_cards (list): list of members of class Card that are community cards
+        deck (list): list with members of class that represent cards in play
 
         Returns:
         float: Returns strength of player's hand
@@ -21,7 +23,7 @@ def hand_strength(hand, comm_cards):
     ourrank = evaluate.rank(hand, comm_cards)
 
     # calculate every possible opponent hand
-    all_possible_oppcards = cards_calculator.opp_starts(hand, comm_cards)
+    all_possible_oppcards = cards_calculator.opp_starts(hand, comm_cards, deck)
     for opp_start in all_possible_oppcards:
         # while you loop through these hand, check how your hand stand against their possible hand
         opprank = evaluate.rank(opp_start, comm_cards)
@@ -35,7 +37,7 @@ def hand_strength(hand, comm_cards):
     handstrength = (ahead + tied / 2) / (ahead + tied + behind)
     return handstrength, ahead, tied, behind
 
-def effective_hand_strength(hand, comm_cards):
+def effective_hand_strength(hand, comm_cards, deck):
     """
         Function that calculates and returns how your hand stands in the game with
         a number representing between 0 and 1 and the amount of hands you beat, lose
@@ -45,6 +47,7 @@ def effective_hand_strength(hand, comm_cards):
         Args:
         hand (list): list of members of class Card that belong to own hand
         comm_cards (list): list of members of class Card that are community cards
+        deck (list): list with members of class that represent cards in play
 
         Returns:
         float: Returns strength of player's hand with more community cards yet to come
@@ -56,7 +59,7 @@ def effective_hand_strength(hand, comm_cards):
     HPTotal = [0, 0, 0]  # Initialize to 0
     ourrank = evaluate.rank(hand, comm_cards)
     # calculate every possible opponent hand
-    all_opp_starts = cards_calculator.opp_starts(hand, comm_cards)
+    all_opp_starts = cards_calculator.opp_starts(hand, comm_cards, deck)
     for oppcards in all_opp_starts:
         # while you loop through these hand, check how your hand stand against their possible hand
         opprank = evaluate.rank(oppcards, comm_cards)
@@ -75,7 +78,7 @@ def effective_hand_strength(hand, comm_cards):
 
         # check what cards can still come in to play and if your best with these cards
         # will beat their best
-        remaing_cards = cards_calculator.cards_left(hand, oppcards, comm_cards)
+        remaing_cards = cards_calculator.cards_left(hand, oppcards, comm_cards, deck)
         ourbest = evaluate.rank(hand, remaing_cards)
         oppbest = evaluate.rank(oppcards, remaing_cards)
         if ourbest[0] > oppbest[0]:
@@ -90,12 +93,12 @@ def effective_hand_strength(hand, comm_cards):
         else:
             HP[index][2] += 1  # behind
 
-    hand_strength = (HPTotal[0] + 0.5 * HPTotal[1]) / sum(number for number in HPTotal)
+    handstrength = hand_strength(hand, comm_cards, deck)
     # calculate the possibilty that while we are ahead now, what is the chance we fall behind
     Ppot = (HP[2][0] + HP[2][1] / 2 + HP[1][0] / 2) / (HPTotal[2] + HPTotal[1])
     # calculate the possibilty that while we are behind now, what is the chance of getting ahead
     Npot = (HP[0][2] + HP[1][2] / 2 + HP[0][1] / 2) / (HPTotal[0] + HPTotal[1])
-    result = hand_strength * (1 - Npot) + (1 - hand_strength) * Ppot
+    result = handstrength[0] * (1 - Npot) + (1 - handstrength[0]) * Ppot
     situations_win = 0
     situations_lose = 0
     situations_tie = 0
@@ -105,7 +108,45 @@ def effective_hand_strength(hand, comm_cards):
         situations_lose += future[2]
     return result, situations_win, situations_tie, situations_lose
 
-def expectiminimax(hand, comm_cards):
+def own_hand_calculator(hand, comm_cards, deck):
+    """
+            Function that calculates and returns how your hand stands in the game with
+            a number representing between 0 and 1 and the amount of hands you beat, lose
+            or tie againsts.
+
+            Args:
+            hand (list): list of members of class Card that belong to own hand
+            comm_cards (list): list of members of class Card that are community cards
+            deck (list): list with members of class that represent cards in play
+
+            Returns:
+            float: Returns strength of player's hand
+            int: Returns amount of hands we are ahead from
+            int: Returns amount of hands we are tied with
+            int: Returns amount of hands we are behind
+        """
+    ahead = tied = behind = 0
+
+    # calculate every possible opponent hand
+    all_possible_oppcards = cards_calculator.opp_starts(hand, comm_cards, deck)
+    for opp_start in all_possible_oppcards:
+        all_possible_board_combis = cards_calculator.potential_full_table(hand, opp_start, comm_cards, deck)
+        # print(len(all_possible_board_combis))
+        for possible_board_combi in all_possible_board_combis:
+            whole_potential_board = comm_cards + possible_board_combi
+            opprank = evaluate.rank(opp_start, whole_potential_board)
+            ourrank = evaluate.rank(hand, whole_potential_board)
+            if ourrank > opprank:
+                ahead += 1
+            elif ourrank == opprank:
+                tied += 1
+            else:
+                behind += 1
+
+    handstrength = (ahead + tied / 2) / (ahead + tied + behind)
+    return handstrength, ahead, tied, behind
+
+def expectiminimax(hand, comm_cards, deck):
     """
         Function that checks if hand strength or effective hand strength is to be calculated
         and returns action to be made by player based on odds.
@@ -113,17 +154,25 @@ def expectiminimax(hand, comm_cards):
         Args:
         hand (list): list of members of class Card that belong to own hand
         comm_cards (list): list of members of class Card that are community cards
+        deck (list): list with members of class that represent cards in play
 
         Returns:
         string: Returns action to be done by player
     """
     if len(comm_cards) == 5:
-        chance = hand_strength(hand, comm_cards)[0]
+        chance = hand_strength(hand, comm_cards, deck)[0]
     else:
-        chance = effective_hand_strength(hand, comm_cards)[0]
+        chance = effective_hand_strength(hand, comm_cards, deck)[0]
     if chance > 0.8:
         return "raise"
     elif chance > 0.5:
         return "check"
     else:
         return "fold"
+
+if __name__ == "__main__":
+    deck = cards.deck()
+    hand = [deck[5], deck[40]]
+    comm_cards = [deck[20], deck[13], deck[15]]
+    print(own_hand_calculator(hand, comm_cards, deck))
+    print(effective_hand_strength(hand, comm_cards, deck))
