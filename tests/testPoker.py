7d69f4a9a5338042bce3d0a52b0b6bba7aaee127
ricomegens <rico.megens@gmail.com>
2023-06-28 02:18:01 +0200

    updated tests with creating an instance of class in test

--- tests/testPoker.py
+++ tests/testPoker.py
@@ -1,50 +1,68 @@
-import poker
+from poker import Poker
 from player import Player
 import unittest
 
 class TestPoker(unittest.TestCase):
     def testPot(self):
+        game = Poker()
         self.assertEqual(game.pot, 0)
     def testUpdatePot(self):
+        game = Poker()
         game.update_pot(20)
         self.assertEqual(game.pot, 20)
     def testResetPot(self):
+        game = Poker()
         game.update_pot(20)
         self.assertEqual(game.pot, 20)
         game.reset_pot()
         self.assertEqual(game.pot, 0)
     def testPlayers(self):
-        self.assertEqual(game.players, [player1, player2])
+        game = Poker()
+        self.assertEqual(game.players, [])
     def testAddPlayer(self):
-        player3 = Player("Test")
-        game.add_player(player3)
-        self.assertEqual(game.players, [player1, player2, player3])
-    def testRemovePlayer(self):
-        game.remove_player(player2)
+        game = Poker()
+        player1 = Player("Rico")
+        game.add_player(player1)
         self.assertEqual(game.players, [player1])
+    def testRemovePlayer(self):
+        game = Poker()
+        player1 = Player("Rico")
+        game.add_player(player1)
+        self.assertEqual(game.players, [player1])
+        game.remove_player(player1)
+        self.assertEqual(game.players, [])
     def testDealHand(self):
+        game = Poker()
+        game.create_deck()
+        player1 = Player("Rico")
+        game.add_player(player1)
         game.deal_hand()
         self.assertEqual(len(player1.hand), 2)
     def testResetHand(self):
+        game = Poker()
+        game.create_deck()
+        player1 = Player("Rico")
+        game.add_player(player1)
         game.deal_hand()
         self.assertEqual(len(player1.hand), 2)
         game.reset_hand()
         self.assertEqual(len(player1.hand), 0)
     def testCommCards(self):
+        game = Poker()
         self.assertEqual(game.comm_cards, [])
     def testDealCommCards(self):
+        game = Poker()
+        game.create_deck()
         game.deal_comm_cards(2)
         self.assertEqual(len(game.comm_cards), 2)
     def testResetCommCards(self):
+        game = Poker()
+        game.create_deck()
         game.deal_comm_cards(2)
         self.assertEqual(len(game.comm_cards), 2)
         game.reset_comm_cards()
         self.assertEqual(len(game.comm_cards), 0)
     def testCreateDeck(self):
+        game = Poker()
         game.create_deck()
         self.assertEqual(len(game.deck), 52)
-
-if __name__ == "__main__":
-    player1 = Player("Rico")
-    player2 = Player("Luffy")
-    game = poker.Poker([player1, player2])
