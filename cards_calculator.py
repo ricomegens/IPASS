6032b01ca6360b814170269f88142ab91b0831bc
ricomegens <rico.megens@gmail.com>
2023-06-28 13:04:23 +0200

    added parameter so it correctly removes known cards

--- cards_calculator.py
+++ cards_calculator.py
@@ -1,21 +1,19 @@
 from itertools import combinations
-import cards
 
-
-def opp_starts(own_cards, comm_cards):
+def opp_starts(own_cards, comm_cards, deck):
     """
         Function that returns a list off all possible combination of cards the
         opponents player may have
 
         Args:
-        own_cards (list): list of members of class Card that belong to own hand
-        comm_cards (list): list of members of class Card that are community cards
+        own_cards (list): list with members of class Card that belong to own hand
+        comm_cards (list): list with members of class Card that are community cards
+        deck (list): list with members of class that we want to remove cards out
 
         Returns:
         list: Returns list of cards combinations
     """
     known = own_cards + comm_cards
-    deck = cards.deck()
     for card in known:
         if card in deck:
             deck.remove(card)
@@ -23,21 +21,33 @@ def opp_starts(own_cards, comm_cards):
     combi = [list(combi) for combi in combis]
     return combi
 
-# def potential_full_table(own_cards, opp_cards, comm_cards, deck):
-#     amount_of_cards_to_come = 5 - len(comm_cards)
-#     known_cards = own_cards + comm_cards
-#     for card in known_cards:
-#         if card in deck:
-#             deck.remove(card)
-#     deck_copy = deck.copy()
-#     for card in opp_cards:
-#         if card in deck:
-#             deck_copy.remove(card)
-#     cards_to_come = list(combinations(deck_copy, amount_of_cards_to_come))
-#     combis = [list(card) for card in cards_to_come]
-#     return combis
+def potential_full_table(own_cards, opp_cards, comm_cards, deck):
+    """
+        Function that returns a list off all possible combination of cards the
+        table may get
 
-def cards_left(own_cards, opp_cards, comm_cards):
+        Args:
+        own_cards (list): list with members of class Card that belong to own hand
+        comm_cards (list): list with members of class Card that are community cards
+        deck (list): list with members of class that we want to remove cards out
+
+        Returns:
+        list: Returns list of cards combinations
+    """
+    amount_of_cards_to_come = 5 - len(comm_cards)
+    known_cards = own_cards + comm_cards
+    for card in known_cards:
+        if card in deck:
+            deck.remove(card)
+    deck_copy = deck.copy()
+    for card in opp_cards:
+        if card in deck:
+            deck_copy.remove(card)
+    cards_to_come = list(combinations(deck_copy, amount_of_cards_to_come))
+    combis = [list(card) for card in cards_to_come]
+    return combis
+
+def cards_left(own_cards, opp_cards, comm_cards, deck):
     """
         Function that returns the list of the remaining cards after removing all
         the known cards (your own hand and community cards) and possible opponent hand
@@ -46,13 +56,13 @@ def cards_left(own_cards, opp_cards, comm_cards):
         own_cards (list): list of members of class Card that belong to own hand
         opp_cards (list): list of members of class Card that the opponent may hold
         comm_cards (list): list of members of class Card that are community cards
+        deck (list): list with members of class that we want to remove cards out
 
         Returns:
         list: Returns list with cards remaining
     """
     # make a list of all the known cards and remove them from the deck
     known_cards = own_cards + comm_cards
-    deck = cards.deck()
     for card in known_cards:
         if card in deck:
             deck.remove(card)
